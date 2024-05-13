482cf5c828880b071faa3d2c7651d119bfc67dd6
Abhishek Neupane <neupaneabhishek13@gmail.com>
2024-05-14 00:49:29 +0545

    animation added

--- flutter_project/lib/screens/splash_screen.dart
+++ flutter_project/lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/screens/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,14 +8,35 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 4000),
+    );
+    _animation = Tween<double>(begin: 1.0, end: 0.0).animate(_controller);
+    _controller.forward();
+
+    _controller.addStatusListener((status) { 
+      if(status == AnimationStatus.completed){
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>const HomeScreen(),),);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Opacity(
-          opacity: 80,
+          opacity: _animation.value,
           child: const Text("Lets get started muji haru"),
         ),
       ),
