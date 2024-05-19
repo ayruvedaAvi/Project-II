bff243ea59a7569a01465d862e3b8000730043d2
Abhishek Neupane <neupaneabhishek13@gmail.com>
2024-05-19 18:09:20 +0545

    added theme color and font

--- flutter_project/lib/my_app.dart
+++ flutter_project/lib/my_app.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/screens/splash_screen.dart';
 
-class MyApp extends StatelessWidget{
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 204, 140, 37),
+    brightness: Brightness.light,
+  ),
+  textTheme: GoogleFonts.robotoMonoTextTheme(),
+);
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: theme,
+      home: const Scaffold(
         body: SplashScreen(),
       ),
     );
