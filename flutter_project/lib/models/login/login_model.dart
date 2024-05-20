bfbf161a91aedadd19c58de93817f815ef78ff26
Abhishek Neupane <neupaneabhishek13@gmail.com>
2024-05-20 23:53:30 +0545

    added login model

---
+++ flutter_project/lib/models/login/login_model.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'login_model.g.dart';
+
+// flutter pub run build_runner build
+
+@JsonSerializable()
+class LoginModel {
+  String? email;
+  String? password;
+
+  LoginModel({
+    this.email,
+    this.password,
+  });
+
+  factory LoginModel.fromJson(Map<String, dynamic> json) =>
+      _$LoginModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LoginModelToJson(this);
+}
