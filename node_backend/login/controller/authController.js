58ac555e5f4376a211e69805fd5287f4a34d0273
Ashutosh9938 <ashutosh.201752@ncit.edu.np>
2024-05-21 14:53:24 +0545

    update back-end

--- node_backend/login/controller/authController.js
+++ node_backend/login/controller/authController.js
@@ -2,12 +2,13 @@ const User= require('../models/user')
 const { StatusCodes }=require('http-status-codes')
 const CustomError=require('../errors')
 const jwt=require('jsonwebtoken')
- const {attachCookiesToResponse}=require('../utils')
+ const { createJWT }=require('../utils')
 
 
 
 const register= async(req,res) =>{
     const {email,name,password}=req.body;
+    
     
     const emailAlreadyExist=await User.findOne({email})
     if(emailAlreadyExist){
@@ -15,12 +16,13 @@ const register= async(req,res) =>{
     }
     const isMyfirstacc=(await User.countDocuments({}))===0
      const role = isMyfirstacc ? 'admin' : 'user';
+     const user=await User.create({name,email,password,role})
+     const tokenUser={name:user.name,iserId:user._id,role:user.role}
+     const token=createJWT({payload:tokenUser})
+     res.status(StatusCodes.CREATED).json({user:tokenUser,token})
 
-    const user=await User.create({name,email,password,role})
 
-const tokenUser={name:user.name,iserId:user._id,role:user.role}
-  attachCookiesToResponse({res,user:tokenUser})
-  res.status(StatusCodes.CREATED).json({user:tokenUser})
+  
 
     
   
@@ -29,27 +31,23 @@ const tokenUser={name:user.name,iserId:user._id,role:user.role}
 
 const login= async(req,res) =>{
     const {email,password}=req.body;
-
+    console.log(req.body)
     if(!email || !password){
         throw new CustomError.BadRequestError("Invalid email and password")
     }
     const user = await User.findOne({email});
     if(!user){
-        throw new CustomError.UnauthenticatedError("Invalid user")
+         throw new CustomError.UnauthenticatedError("Invalid user")
     }
     const isPasswordCorrect=await user.comparePassword(password)
     if(!isPasswordCorrect){
-        throw new CustomError.UnauthenticatedError("Password Incorrect")
+        throw new CustomError.UnauthenticatedError("Invalid password")
     }
     const tokenUser={name:user.name,iserId:user._id,role:user.role}
-  attachCookiesToResponse({res,user:tokenUser})
-  res.status(StatusCodes.CREATED).json({user:tokenUser})
+    const token=createJWT({payload:tokenUser})
+  res.status(StatusCodes.CREATED).json({user:tokenUser,token})
 
 };
-
-
-
-
 
 
 const logout= async(req,res) =>{
