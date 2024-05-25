399e88d705d851d9f6e6358113e32a00188d623b
Abhishek Neupane <neupaneabhishek13@gmail.com>
2024-05-25 18:49:15 +0545

    added shared preference

---
+++ flutter_project/lib/utils/shared_preferences/shared_preference.dart
@@ -0,0 +1,44 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserSharedPreference{
+  
+  static Future<void> saveDataToStorage(String key, value) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
+    if (value is int) {
+      await prefs.setInt(key, value);
+    } else if (value is String) {
+      await prefs.setString(key, value);
+    } else if (value is bool) {
+      await prefs.setBool(key, value);
+    } else {
+      // Handle other cases (e.g., null)
+      await prefs.setDouble(key, value);
+    }
+  }
+
+  static void removeDataFromStorage(key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove(key);
+  }
+
+  static Future<String?> getStringDataFromStorage(key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  static Future<int?> getIntDataFromStorage(String key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(key);
+  }
+
+  static Future<bool?> getBoolDataFromStorage(String key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(key);
+  }
+
+  static Future<double?> getDoubleDataFromStorage(String key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getDouble(key);
+  }
+}
