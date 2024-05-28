fa51b1c2340751f5d1cac885f1a033cb609294a7
Kamal-pandey03 <kamal.201712#ncit.edu.np>
2024-05-28 16:24:41 +0545

    user profile ui completed

---
+++ flutter_project/lib/screens/addpost_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddpostScreen extends StatefulWidget {
+  const AddpostScreen({super.key});
+
+  @override
+  State<AddpostScreen> createState() => _AddpostScreenState();
+}
+
+class _AddpostScreenState extends State<AddpostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
