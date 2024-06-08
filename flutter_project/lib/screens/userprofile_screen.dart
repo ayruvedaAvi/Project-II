997681e8d6160dd5aed2f367c0fcb2ff485d05d8
Kamal-pandey03 <kamal.201712#ncit.edu.np>
2024-06-08 17:59:51 +0545

    profile screen updated with cards

--- flutter_project/lib/screens/userprofile_screen.dart
+++ flutter_project/lib/screens/userprofile_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/screens/authScreens/login_screen.dart';
+// import 'package:flutter_project/screens/authScreens/login_screen.dart';
 import 'package:flutter_project/utils/shared_preferences/shared_preference.dart';
-import 'package:flutter_project/widgets/custom_sizedbox_button.dart';
+import 'package:flutter_project/widgets/custom_profiledetailscard.dart';
+// import 'package:flutter_project/widgets/custom_sizedbox_button.dart';
 import 'package:get/get.dart';
-// import 'package:get/get.dart';
 
 class UserprofileScreen extends StatefulWidget {
   const UserprofileScreen({super.key});
@@ -32,110 +32,117 @@ class _UserprofileScreenState extends State<UserprofileScreen> {
         backgroundColor: Colors.white,
         body: Center(
           child: Padding(
-              padding: const EdgeInsets.only(left: 25, right: 25),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // const Row(
-                  //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  //   children: [
-                  //     Icon(
-                  //       Icons.arrow_back,
-                  //       color: Colors.black,
-                  //     ),
-                  //     Icon(
-                  //       Icons.sunny,
-                  //       color: Colors.black,
-                  //     ),
-                  //   ],
-                  // ),
-                  const CircleAvatar(
-                    backgroundImage: AssetImage('assets/images/boy_image.jpg'),
-                    radius: 60,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Obx(
-                        () => Text(
-                          name.value,
-                          style: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
+            padding: const EdgeInsets.only(left: 25, right: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // const Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //   children: [
+                //     Icon(
+                //       Icons.arrow_back,
+                //       color: Colors.black,
+                //     ),
+                //     Icon(
+                //       Icons.sunny,
+                //       color: Colors.black,
+                //     ),
+                //   ],
+                // ),
+                const CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/boy_image.jpg'),
+                  radius: 60,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Obx(
+                      () => Text(
+                        name.value,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
                       ),
-                      const SizedBox(
-                        width: 7,
-                      ),
-                      const Icon(
-                        Icons.verified,
-                        color: Colors.deepPurple,
-                        size: 20,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 7,
-                  ),
-                  const Text(
-                    "9876543210",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  CustomSizedboxButton(
-                    prefixIcon: Icons.history,
-                    labelText: "Work History",
-                    onTap: () {},
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  CustomSizedboxButton(
-                    prefixIcon: Icons.privacy_tip_outlined,
-                    labelText: "Privacy & Security",
-                    onTap: () {},
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  CustomSizedboxButton(
-                    prefixIcon: Icons.person_pin,
-                    labelText: "Invite a Friend",
-                    onTap: () {},
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  CustomSizedboxButton(
-                    prefixIcon: Icons.settings,
-                    labelText: "Setting",
-                    onTap: () {},
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  CustomSizedboxButton(
-                    prefixIcon: Icons.help_outline,
-                    labelText: "Help & Support",
-                    onTap: () {},
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  CustomSizedboxButton(
-                    prefixIcon: Icons.logout_outlined,
-                    labelText: "Logout",
-                    onTap: () {
-                      UserSharedPreference.removeDataFromStorage('token');
-                      Get.off(() => const LoginScreen());
-                    },
-                  ),
-                ],
-              )),
+                    ),
+                    const SizedBox(
+                      width: 7,
+                    ),
+                    const Icon(
+                      Icons.verified,
+                      color: Colors.deepPurple,
+                      size: 20,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 7,
+                ),
+                const Text(
+                  "9876543210",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomProfiledetailscard(
+                      name: "Pending Jobs",
+                      contextIcon: Icons.pending_actions_outlined,
+                      numCount: "25",
+                    ),
+                    CustomProfiledetailscard(
+                      name: "Active Jobs",
+                      contextIcon: Icons.directions_run_rounded,
+                      numCount: "15",
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomProfiledetailscard(
+                      name: "Completed Jobs",
+                      contextIcon: Icons.check_circle_outline,
+                      numCount: "10",
+                    ),
+                    CustomProfiledetailscard(
+                      name: "Refer a friend",
+                      contextIcon: Icons.pending_actions_outlined,
+                      numCount: "25",
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomProfiledetailscard(
+                      name: "Pending Jobs",
+                      contextIcon: Icons.pending_actions_outlined,
+                      numCount: "25",
+                    ),
+                    CustomProfiledetailscard(
+                      name: "Logout",
+                      contextIcon: Icons.logout_outlined,
+                      numCount: "Bye!",
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+              ],
+            ),
+          ),
         ));
   }
 }
---
+++ flutter_project/lib/widgets/custom_profiledetailscard.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class CustomProfiledetailscard extends StatelessWidget {
+  final String? name;
+  final String? numCount;
+  final IconData? contextIcon;
+  const CustomProfiledetailscard(
+      {super.key, this.contextIcon, this.name, this.numCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 143, 69, 211),
+          borderRadius: BorderRadius.circular(10)),
+      height: 130,
+      width: 160,
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 233, 218, 250),
+            borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0xFF000000)
+                    .withOpacity(0.5), // Semi-transparent black
+                spreadRadius: 0,
+                blurRadius: 15,
+                offset: const Offset(0, 5),
+              )
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  numCount.toString(),
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 58, 18, 89),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromARGB(255, 192, 146, 238),
+                    ),
+                    padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                    child: Icon(
+                      contextIcon,
+                      color: const Color.fromARGB(255, 58, 18, 89),
+                    )),
+              ],
+            ),
+            Text(
+              name.toString(),
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 58, 18, 89),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
